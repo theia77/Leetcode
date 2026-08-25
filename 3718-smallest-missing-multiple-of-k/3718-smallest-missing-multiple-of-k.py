9cbda84da66505f6dd7d9b4fc082c9913ca1d13e
theia77 <sumoxoxo9@gmail.com>
2026-08-25 09:50:47 +0530

    Time: 5 ms (10.34%), Space: 12.4 MB (50.57%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums=list(set(nums))
+        nums.sort()
+        d={}
+        for i in range(len(nums)):
+            if nums[i]%k==0:
+                key=nums[i]//k
+                value=nums[i]
+                d[key]=value
+        num=list(d.keys())
+        output=[]
+        i=1
+        while True:
+            if i not in num:
+                return i*k
+                break
+            else: 
+                i=i+1
