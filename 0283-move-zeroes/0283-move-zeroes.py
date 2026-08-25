590644f457320ed6ef0fdf1f00c339dbbf71c1da
theia77 <sumoxoxo9@gmail.com>
2026-08-25 12:04:43 +0530

    Time: 11 ms (25.14%), Space: 13.8 MB (20.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+                
